dbd84752f5b905ac20c1255c3310e0c09f71704e
Pedro <dev.akcio@gmail.com>
2022-10-06 21:02:05 -0300

    Criado atribudo de intância, nome e idade

--- oo/Pessoa.py
+++ oo/Pessoa.py
@@ -1,10 +1,15 @@
 class Pessoa:
+    def __init__(self,nome, idade):
+        self.idade = idade
+        self.nome = nome
+
+
     def cumprimentar(self):
         return f'Ola {id(self)}'
 if __name__== '__main__' :
-    p = Pessoa()
+    p = Pessoa('Pedro',20)
     print(Pessoa.cumprimentar(p))
-
     print(id(p))
-
     print(p.cumprimentar())
+    print(p.nome)
+    print(p.idade)
